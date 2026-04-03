361059e78b9274e0482250d213d57d13f5c95485
oszo193iy-oss <os.zo193iy@gmail.com>
2026-04-03 10:58:29 +0900

    テキストレビュー

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -14,7 +14,7 @@ class TodoController extends Controller
     }
 
     public function index()
-    {
+    { 
          $todo = new Todo();
          $todos = $todo->all();
          return view('todo.index', ['todos' => $todos]);
@@ -23,12 +23,12 @@ class TodoController extends Controller
     public function store(Request $request) 
 {
     $inputs = $request->all(); 
-    dd($inputs);
 
     $todo = new Todo(); 
     $todo->fill($inputs);
     $todo->save();
 
     return redirect()->route('todo.index');
+    // 処理が終わった後に「指定した名前のルート（ニックネーム）へ画面を切り替える
 }
 }
